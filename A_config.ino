dfd3b0196cfc9e4a708f0aff1b3a3be7371e59ca
ahakkoca07 <ahakkoca07@gmail.com>
2024-09-23 17:57:23 +0300

    Added quick wake

--- A_config.ino
+++ A_config.ino
@@ -21,10 +21,12 @@
 #define NUM_LEDS 2  //Neopixel count
 
 //----------Define boot settings---------//
+//If quick wake is defined, it will use the values below to
+//speed up the booting proccess, note that it will switch to
+//the values in the settings.
 #define QUICK_WAKE  //If quick wake is defined, it will use the values below to speed the wake up
 
-#ifdef QUICK_WAKE  //Checks if it is defined
-#define language 0  //Copy the contents of the preferred language into language.ino file
+#ifdef QUICK_WAKE       //Checks if it is defined
 #define brightness 255  //Set the value between 0-255
-#define style 0  //default style
-#define hour24 true  //Checks 12/24 hour style; if true, uses 24 hours.
+#define style 0         //default style
+#define hour24 true     //Checks 12/24 hour style; if true, uses 24 hours.
--- language.ino
+++ language.ino
@@ -1,20 +1,37 @@
 //Translation template, please don't delete the comments for others to understand
+if (language = "English") {
+  const LANG = "en";  //Language Name
 
-const LANG = "en";  //Language Name
 
-//-------------------Main Menu-------------------//
-const SLEEP = "Sleep";  //Sleep
-const CUSTO = "Customize";  //Customise
-const NEOPI = "Neopixels";  //Neopixels
-const TOOLS = "Tools";  //Tools
-const WIFIF = "Wifi Functions";  //Wifi Functions
-const CONFO = "Configure OLED";  //COnfigure OLED
-const TIMEW = "Time & Weather";  //Time & Weather
-const SETTI = "Settings";  //Settings
+  //-------------------Weekdays-------------------//
+  const MON = "Monday";
+  const TUE = "Tuesday";
+  const WED = "Wednesday";
+  const THU = "Thursday";
+  const FRI = "Friday";
+  const SAT = "Saturday";
+  const SUN = "Sunday";
 
-//---------------Customize Sub-Menu--------------//
-const WATCS = "Watch Screen";  //Watch Screen
-const WEATS = "Weather Screen";  //Weather Screen
-const MENUS = "Menu Screen";  //Menu Screen
+  const YES = "Yes";
+  const NO = "No";
+  const OK = "OK";
+  const ON = "On";
+  const OFF = "Off";
 
-//-------------------Main Menu-------------------//
+  //-------------------Main Menu-------------------//
+  const SLEEP = "Sleep";           //Sleep
+  const CUSTO = "Customize";       //Customise
+  const NEOPI = "Neopixels";       //Neopixels
+  const TOOLS = "Tools";           //Tools
+  const WIFIF = "Wifi Functions";  //Wifi Functions
+  const CONFO = "Configure OLED";  //COnfigure OLED
+  const TIMEW = "Time & Weather";  //Time & Weather
+  const SETTI = "Settings";        //Settings
+
+  //---------------Customize Sub-Menu--------------//
+  const WATCS = "Watch Screen";    //Watch Screen
+  const WEATS = "Weather Screen";  //Weather Screen
+  const MENUS = "Menu Screen";     //Menu Screen
+
+  //-------------------Main Menu-------------------//
+}
